3b3c3b6a5be076918c41ef81c7fbcd23ce0ce58f
tyoko010 <547301+tyoko010@users.noreply.github.com>
2025-03-01 23:31:57 +0900

    Changed to install packages first.

--- install.sh
+++ install.sh
@@ -3,20 +3,6 @@ set -e
 
 DOTFILES_DIR=$(cd $(dirname $0) && pwd)
 
-function create_link() {
-  local origin=$1
-  local link=$2
-  if [ -e $link ]; then
-    echo "Already exists: $link"
-  else
-    ln -s $origin $link
-    echo "Created symbolic link: $link"
-  fi
-}
-
-create_link $DOTFILES_DIR/plugins.toml ~/.config/sheldon/plugins.toml
-create_link $DOTFILES_DIR/zshrc ~/.zshrc
-
 BREW_PACKAGES=(
   fzf
   sheldon
@@ -30,17 +16,37 @@ BREW_PACKAGES=(
 
 CASK_APPS=(
   iterm2
+  font-fira-code-nerd-font
 )
 
-if ! command -v brew &> /dev/null; then
-  echo "Homebrew is not installed. Please install Homebrew first."
-  exit 1
-fi
+function install_brew_packages() {
+  if ! command -v brew &> /dev/null; then
+    echo "Homebrew is not installed. Please install Homebrew first."
+    exit 1
+  fi
 
-brew update
+  brew update
 
-echo "Installing packages..."
-brew install "${BREW_PACKAGES[@]}"
+  echo "Installing packages..."
+  brew install "${BREW_PACKAGES[@]}"
 
-echo "Installing cask apps..."
-brew install --cask "${CASK_APPS[@]}"
+  echo "Installing cask apps..."
+  brew install --cask "${CASK_APPS[@]}"
+}
+
+function create_link() {
+  local origin=$1
+  local link=$2
+  if [ -e $link ]; then
+    echo "Already exists: $link"
+  else
+    ln -s $origin $link
+    echo "Created symbolic link: $link"
+  fi
+}
+
+install_brew_packages
+create_link $DOTFILES_DIR/plugins.toml ~/.config/sheldon/plugins.toml
+create_link $DOTFILES_DIR/zshrc ~/.zshrc
+
+
